f964e3fc529ec66356c7f81bb3b2cb126cb51d80
Larissa Andrade <44838761+lariandrade@users.noreply.github.com>
2021-01-20 12:20:00 -0300

    exercicio 01

---
+++ slnEX01/prj_EX01/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace prj_EX01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpInformacoes = new System.Windows.Forms.GroupBox();
+            this.txtPesoPeixe = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnVerifica = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.gpInformacoes.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpInformacoes
+            // 
+            this.gpInformacoes.BackColor = System.Drawing.SystemColors.Control;
+            this.gpInformacoes.Controls.Add(this.txtPesoPeixe);
+            this.gpInformacoes.Controls.Add(this.label1);
+            this.gpInformacoes.Location = new System.Drawing.Point(12, 25);
+            this.gpInformacoes.Name = "gpInformacoes";
+            this.gpInformacoes.Size = new System.Drawing.Size(376, 174);
+            this.gpInformacoes.TabIndex = 0;
+            this.gpInformacoes.TabStop = false;
+            this.gpInformacoes.Text = "Informações";
+            // 
+            // txtPesoPeixe
+            // 
+            this.txtPesoPeixe.Location = new System.Drawing.Point(209, 70);
+            this.txtPesoPeixe.Name = "txtPesoPeixe";
+            this.txtPesoPeixe.Size = new System.Drawing.Size(100, 26);
+            this.txtPesoPeixe.TabIndex = 1;
+            this.txtPesoPeixe.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ApenasNumeros);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Peso dos Peixes";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 311);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(376, 174);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resultados";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(215, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Label 5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Label 4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Multa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Excesso";
+            // 
+            // btnVerifica
+            // 
+            this.btnVerifica.Location = new System.Drawing.Point(85, 244);
+            this.btnVerifica.Name = "btnVerifica";
+            this.btnVerifica.Size = new System.Drawing.Size(75, 31);
+            this.btnVerifica.TabIndex = 3;
+            this.btnVerifica.Text = "Verificar";
+            this.btnVerifica.UseVisualStyleBackColor = true;
+            this.btnVerifica.Click += new System.EventHandler(this.BtnVerifica);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(213, 244);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 31);
+            this.btnNew.TabIndex = 4;
+            this.btnNew.Text = "Novo";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.BtnNew);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 509);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnVerifica);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gpInformacoes);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "PeixeSoft 1.0";
+            this.gpInformacoes.ResumeLayout(false);
+            this.gpInformacoes.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpInformacoes;
+        private System.Windows.Forms.TextBox txtPesoPeixe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnVerifica;
+        private System.Windows.Forms.Button btnNew;
+    }
+}
+
---
+++ slnEX01/prj_EX01/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prj_EX01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ApenasNumeros(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar) || e.KeyChar == (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+        }
+
+ 
+        private void BtnVerifica(object sender, EventArgs e)
+        {
+            int peso = int.Parse(txtPesoPeixe.Text);
+            int multa = 0;
+            int aumento = 0;
+
+            if(peso <= 50)
+            {
+                label5.Text = "ZERO";
+                label4.Text = "ZERO";
+            }
+            else if(peso > 50)
+            {
+
+                aumento = peso - 50;
+                multa = aumento * 4;
+                label4.Text = aumento.ToString() + " kg";
+                label5.Text = "R$ " + multa.ToString();
+            }
+        }
+
+        private void BtnNew(object sender, EventArgs e)
+        {
+            txtPesoPeixe.Text = "";
+            txtPesoPeixe.Focus();
+            label4.Text = "";
+            label5.Text = "";
+        }
+
+    }
+}
